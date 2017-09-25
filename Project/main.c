1bce389cde188f171caea345d7342f2555b24927
Lucas Pereira Lopes <lkogici@gmail.com>
2017-09-24 23:01:01 -0300

    Final do teste
    	O teste foi criar um novo branch e altera-lo, afim de visualizar
    se as mudanças no branch principal seriam afetadas.A conclusão foi a de
    que os branchs não estão linkados ou seja, um não altera o outro.
    
    Signed-off-by: Lucas Pereira Lopes <lkogici@gmail.com>

--- Project/main.c
+++ Project/main.c
@@ -186,7 +186,6 @@ int main(){
     }
     printf("\n Objetivo encontrado\n");
     printf("    Pressione qualquer tecla para sair\n" );
-    printf("Branch Master \n");
     libera_lista(listaAberta);
     libera_lista(listaFechada);
     getche();
